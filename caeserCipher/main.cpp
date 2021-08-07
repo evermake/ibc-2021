56e6ee14ce8d6804bf70735fb2a2d3a853701393
evermake <vladeryabkin@gmail.com>
2021-08-07 16:34:59 +0300

    Caeser cipher written

---
+++ caeserCipher/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+    string text;
+    int offset;
+
+    cout << "Type text:" << endl;
+    getline(cin, text);
+
+    string generated = text;
+
+    cout << "Type offset:" << endl;
+    cin >> offset;
+
+    if (0 > offset or offset > 26) {
+        cout << "Incorrect offset!" << endl;
+    }
+
+    for (int i = 0; i < text.length(); i++) {
+        int ch = (unsigned char) text[i];
+
+        if (ch >= 97 and ch <= 122) {
+            // Edit lowercase letters
+            ch -= 97;
+            ch += offset;
+            ch %= 26;
+            ch += 97;
+        } else if (ch >= 65 and ch <= 90) {
+            // Edit uppercase letters
+            ch -= 65;
+            ch += offset;
+            ch %= 26;
+            ch += 65;
+        }
+        // Other symbols will not be changed
+
+        generated[i] = ch;
+    }
+
+    for (char c : generated) {
+        cout << c;
+    }
+}
+
